392866dc39852792623aeba0c33226898704bbec
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-07-08 16:20:59 -0700

    Added view button, need to add logic for it. Make drop down menu work

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,17 +1,18 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { ReactComponent as PlusSign } from '../images/PlusSign.svg';
 import DropDownContact from "./ContactButtons";
 
 const ContactList = ({ contacts, updateContact, updateCallback, setQuery, openCreateModal }) => {
-  // Function to delete a contact
+  const [openDropdown, setOpenDropdown] = useState(null);
+
+  const toggleDropdown = (id) => {
+    setOpenDropdown(openDropdown === id ? null : id);
+  };
+
   const onDelete = async (id) => {
     try {
-      const options = {
-        method: "DELETE"
-      };
-      // Send a delete request to the server
+      const options = { method: "DELETE" };
       const response = await fetch(`http://localhost:8080/contact/${id}`, options);
-      // If the request is successful, update the contacts list
       if (response.status === 200) {
         updateCallback();
       } else {
@@ -22,39 +23,28 @@ const ContactList = ({ contacts, updateContact, updateCallback, setQuery, openCr
     }
   };
 
-  const [openDropdown, setOpenDropdown] = useState(null); // Initialize the state to null
-
-  const toggleDropdown = (id) => {
-    setOpenDropdown(openDropdown === id ? null : id); // Toggle the dropdown based on the clicked contact's id
-  };
-
   return (
     <div>
       <h1 className="center-top-text">Welcome!</h1>
       <h2 className="center-top-text">Leave your contact info!</h2>
       <h2>Contacts</h2>
       <input className="search-create-section" type="text" onChange={(e) => setQuery(e.target.value)} placeholder="Search contacts" />
-      <PlusSign className = "plus-sign" onClick={openCreateModal}></PlusSign>
+      <PlusSign className="plus-sign" onClick={openCreateModal}></PlusSign>
       <table>
         <tbody>
           {contacts.map((contact) => (
             <tr key={contact.id}>
               <td>
-           
-              <div className = "menu-trigger">
-              <button className="contact-name-buttons" onClick={() => toggleDropdown(contact.id)}>
+                <div className="menu-trigger">
+                  <button className="contact-name-buttons" onClick={() => toggleDropdown(contact.id)}>
                     {contact.firstName} {contact.lastName}
                   </button>
                 </div>
-                <div className={`dropdown-menu ${openDropdown === contact.id ? 'active' : 'inactive'}`}>
-                <DropDownContact text = {"Edit"}  onClick={() => updateContact(contact)} />
-                <DropDownContact text = {"Delete"} onClick={() => onDelete(contact.id)} />
+                <div className={`dropdown-menu ${openDropdown === contact.id ? 'active' : ''}`}>
+                  <DropDownContact text={"View"} />
+                  <DropDownContact text={"Edit"} onClick={() => updateContact(contact)} />
+                  <DropDownContact text={"Delete"} onClick={() => onDelete(contact.id)} />
                 </div>
-            
-
-              </td>
-              <td>
-            
               </td>
             </tr>
           ))}
