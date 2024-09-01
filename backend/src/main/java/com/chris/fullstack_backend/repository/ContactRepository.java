91a575795ed27aca6ca0a3a414eeb56c25fc8020
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-08-31 22:28:05 -0700

    Updated SupaBase Issues

--- backend/src/main/java/com/chris/fullstack_backend/repository/ContactRepository.java
+++ backend/src/main/java/com/chris/fullstack_backend/repository/ContactRepository.java
@@ -2,7 +2,8 @@ package com.chris.fullstack_backend.repository;
 
 import com.chris.fullstack_backend.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
